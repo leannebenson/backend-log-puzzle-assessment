0f7122d2c7381a7ff7a8b620dbf0032fb7ca31c7
Leanne Benson <stylistleannerowe@gmail.com>
2020-06-30 17:00:05 -0400

    have been really sick committing just in case

--- logpuzzle.py
+++ logpuzzle.py
@@ -69,14 +69,7 @@ def read_urls(filename):
 
 
 def download_images(img_urls, dest_dir):
-    """Given the URLs already in the correct order, downloads
-    each image into the given directory.
-    Gives the images local filenames img0, img1, and so on.
-    Creates an index.html in the directory with an <img> tag
-    to show each local image file.
-    Creates the directory if necessary.
-    """
-    # +++your code here+++
+
     pass
 
 
